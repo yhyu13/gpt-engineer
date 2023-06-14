84daedc28f79cf34db05ba8b26e1550e9c3be0b2
yhyu13 <yohan680919@gmail.com>
2023-06-14 21:13:51 +0800

    Add .env support

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ from ai import AI
 from db import DB, DBs
 from steps import STEPS
 
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY") 
+# Check if OPENAI_API_KEY is defined
+if api_key == None:
+    # Raise an error if OPENAI_API_KEY is not defined
+    raise ValueError("OPENAI_API_KEY is not defined")
+else:
+    # Set the OPENAI_API_KEY to the api_key variable
+    openai.api_key = api_key
+
+# Get the OPENAI_API_BASE
+api_base = os.getenv("OPENAI_API_BASE")
+# Check if OPENAI_API_BASE is defined
+if api_base != None:   
+    # Set the OPENAI_API_BASE to the api_base variable
+    openai.api_base = api_base
+  
+# Get the GPT model from the environment variable
+gpt_model = os.getenv("gpt_model")
+# Get the GPT temperature from the environment variable
+gpt_temperature = os.getenv("gpt_temperature")
+
 app = typer.Typer()
 
 
@@ -17,8 +43,8 @@ def chat(
         "",
         help="run prefix, if you want to run multiple variants of the same project and later compare them",
     ),
-    model: str = "gpt-4",
-    temperature: float = 0.1,
+    model: str = gpt_model,
+    temperature: float = gpt_temperature,
 ):
     if project_path is None:
         project_path = str(pathlib.Path(__file__).parent / "example")
